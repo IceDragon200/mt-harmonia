172c01c051a203108ff55d800ff5f87a73d098f8
Corey Powell <mistdragon100@gmail.com>
2021-11-01 23:31:19 -0500

    harmonia_element: Element API

--- harmonia_element/api.lua
+++ harmonia_element/api.lua
@@ -1,27 +1,90 @@
--- Register the element blueprint registry for players
--- Each player may have different blueprints unlocked
-nokore.player_data_service:register_domain("element_blueprints", {
+-- @namespace harmonia.element
+local DATA_DOMAIN = "element_blueprints"
+
+local player_data_service = assert(nokore.player_data_service)
+--local player_service = assert(nokore.player_service)
+
+player_data_service:register_domain(DATA_DOMAIN, {
   save_method = "marshall",
 })
 
 harmonia = rawget(_G, "harmonia") or {}
 
-harmonia.registered_element_blueprints = {}
+harmonia.element = {}
 
--- Unlocks an element blueprint for a specific player
+-- @type registered_element_blueprints: {
+--   [blueprint_id: String]: (item_name: String)
+-- }
+harmonia.element.registered_element_blueprints = {}
+
+-- Toggles an element blueprint for a specific player
 --
--- @spec #unlock_player_element_blueprint(player_name: String, blueprint_id: String): void
-function harmonia:unlock_player_element_blueprint(player_name, blueprint_id)
-  nokore.player_data_service:with_player_domain_kv(player_name, "element_blueprints", function (kv_store)
-    kv_store:put(blueprint_id, true)
+-- @spec #toggle_player_element_blueprint(
+--         player_name: String,
+--         blueprint_id: String,
+--         value: Boolean | nil
+--       ): Boolean
+function harmonia.element:toggle_player_element_blueprint(player_name, blueprint_id, value)
+  return player_data_service:with_player_domain_kv(player_name, DATA_DOMAIN, function (kv_store)
+    kv_store:put(blueprint_id, value)
+    return true
   end)
 end
 
+-- Unlocks an element blueprint for a specific player.
+-- Returns true if the blueprint was unlocked, false if inaccessible.
+--
+-- @spec #unlock_player_element_blueprint(player_name: String, blueprint_id: String): Boolean
+function harmonia.element:unlock_player_element_blueprint(player_name, blueprint_id)
+  return self:toggle_player_element_blueprint(player_name, blueprint_id, true)
+end
+
+-- Locks a player's blueprint.
+-- Returns true if the blueprint was locked, false if inaccesible.
+--
+-- @spec #lock_player_element_blueprint(player_name: String, blueprint_id: String): Boolean
+function harmonia.element:lock_player_element_blueprint(player_name, blueprint_id)
+  return self:toggle_player_element_blueprint(player_name, blueprint_id, nil)
+end
+
+-- Determines if a player has a specific blueprint.
+--
+-- @spec #player_has_element_blueprint(player_name: String, blueprint_id: String): Boolean
+function harmonia.element:player_has_element_blueprint(player_name, blueprint_id)
+  local kv = player_data_service:get_player_domain_kv(player_name, DATA_DOMAIN)
+
+  if kv then
+    return kv:get(blueprint_id) == true
+  end
+
+  return false
+end
+
+-- Retrieves the blueprint unlock map for the specified player
+-- Note that the table returned is the raw underlying key-value map.
+-- Under no circumstance should the caller try to modify this table as it can
+-- compromise the integrity of the key-value store.
+--
+-- @spec #get_player_element_blueprints(player_name: String): Table | nil
+function harmonia.element:get_player_element_blueprints(player_name)
+  local kv = player_data_service:get_player_domain_kv(player_name, DATA_DOMAIN)
+
+  if kv then
+    return kv.data
+  end
+
+  return nil
+end
+
+--
+-- private sauce
+--
+
 local function register_element_blueprint(blueprint_id, name)
-  if harmonia.registered_element_blueprints[blueprint_id] then
+  if harmonia.element.registered_element_blueprints[blueprint_id] then
     minetest.log("warn", "blueprint_id=" .. blueprint_id .. " already registered as name=" .. name)
   end
-  harmonia.registered_element_blueprints[blueprint_id] = name
+  harmonia.element.registered_element_blueprints[blueprint_id] = name
 end
 
 local function backfill_element_blueprint_ids()
