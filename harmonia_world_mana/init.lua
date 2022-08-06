a97aba8505dffaa2124c5306135a82f0ae7d443d
Corey Powell <mistdragon100@gmail.com>
2022-08-05 23:35:46 -0500

    harmonia_world_mana: Slower global mana steps
    Every second, versus as-fast-as-possible

--- harmonia_world_mana/init.lua
+++ harmonia_world_mana/init.lua
@@ -11,11 +11,28 @@ local mod = foundation.new_module("harmonia_world_mana", "0.1.0")
 
 mod:require("api.lua")
 
-assert(nokore.block_data_service, "missing block_data_service, did nokore_block_data load?")
+local block_data_service = assert(nokore.block_data_service, "missing block_data_service, did nokore_block_data load?")
+--local reduce_blocks = assert(block_data_service.reduce_blocks)
+
+local elapsed = 0
+
 nokore_proxy.register_globalstep("hamornia_world_mana.update/1", function (delta)
-  nokore.block_data_service:reduce_blocks(0, function (block_id, block, acc)
-    local mana = block.kv:get("mana", 0)
-    block.kv:put("mana", mana + 1)
-    return acc, false
-  end)
+  elapsed = elapsed + delta
+
+  if elapsed > 1 do
+    elapsed = elapsed - 1
+
+    local mana
+    local kv
+    --reduce_blocks(block_data_service, 0, function (block_id, block, acc)
+    -- this is an optimization specifically for harmonia, you should not assume
+    -- that the block service's internal structure will remain the same.
+    for _id,block in pairs(block_data_service.blocks) do
+      kv = block.kv
+      mana = kv:get("mana", 0)
+      kv:put("mana", mana + 1)
+      --return acc, false
+    end
+    --end)
+  end
 end)
