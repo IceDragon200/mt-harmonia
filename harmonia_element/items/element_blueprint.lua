d3dfbfb9a50b7f33114fb650ed85f31f6c0a0902
Corey Powell <mistdragon100@gmail.com>
2022-08-07 23:06:40 -0500

    harmonia_element: Added missing element blueprint textures
    The idea is that element is more sci-fi in its design

--- harmonia_element/items/element_blueprint.lua
+++ harmonia_element/items/element_blueprint.lua
@@ -1,8 +1,20 @@
 local mod = harmonia_element
 
 -- A stock blueprint will not have any attached item and is therefore useless.
+mod:register_tool("element_blueprint_blank", {
+  description = mod.S("Element Blueprint (Blank)"),
+
+  inventory_image = "harmonia_element_blueprint.blank.png",
+})
+
 mod:register_tool("element_blueprint", {
   description = mod.S("Element Blueprint"),
 
-  inventory_image = "element_blueprint.png",
+  inventory_image = "harmonia_element_blueprint.png",
+})
+
+mod:register_tool("element_blueprint_unidentified", {
+  description = mod.S("Element Blueprint (Unidentified)"),
+
+  inventory_image = "harmonia_element_blueprint.unidentified.png",
 })
