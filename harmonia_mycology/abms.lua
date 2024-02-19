085ed78ae72e73c961663d45c75facbeb99d9115
Corey Powell <mistdragon100@gmail.com>
2024-02-19 09:19:15 -0500

    harmonia_mycology: block_pos_to_block_id > node_pos_to_block_id

--- harmonia_mycology/abms.lua
+++ harmonia_mycology/abms.lua
@@ -10,12 +10,12 @@ minetest.register_abm({
 
   action = function (pos, node)
     local nodedef = minetest.registered_nodes[node.name]
-    local meta = minetest.get_meta(pos)
+    -- local meta = minetest.get_meta(pos)
 
-    local mana = meta:get_float("mana")
+    -- local mana = meta:get_float("mana")
     local mana_regen = assert(nodedef.harmonia.mana_regen)
 
-    local block_id = harmonia_world_mana.block_pos_to_block_id(pos)
+    local block_id = harmonia_world_mana.node_pos_to_block_id(pos)
 
     harmonia_world_mana.add_mana_in_block(block_id, mana_regen)
   end,
